9434707d51a5f2573f83a792d2bb090bf6904033
juyomo <juyomo@users.noreply.github.com>
2025-06-12 17:45:20 +0900

    Create 2025_06_17_2_middle_of_the_linked_list.js

---
+++ 2025_06_17_2_middle_of_the_linked_list.js
@@ -0,0 +1,33 @@
+// Author: Juyoung Moon
+// https://leetcode.com/problems/middle-of-the-linked-list/
+// Solved for LeetCode 알고리즘 스터디 (Due Tue, June 17, 2025)
+
+// HW are the following 5 problems:
+// 1. Diameter of Binary Tree
+// 2. Middle of the Linked List
+// 3. Maximum Depth of Binary Tree
+// 4. Contains Duplicate
+// 5. Roman to Integer
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while(fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+};
