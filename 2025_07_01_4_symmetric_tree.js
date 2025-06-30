66b99f3aea3e7e7304015f953123a559040ddd15
juyomo <juyomo@users.noreply.github.com>
2025-06-30 13:34:52 +0900

    Update 2025_07_01_4_symmetric_tree.js

--- 2025_07_01_4_symmetric_tree.js
+++ 2025_07_01_4_symmetric_tree.js
@@ -8,3 +8,32 @@
 // 3. #283 Move Zeroes
 // 4. #101 Symmetric Tree
 // 5. #268 Missing Number
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+function recur(left, right) {
+    if (left == null && right == null) {
+        return true;
+    } else if (left == null || right == null) {
+        return false;
+    } else if (left.val != right.val) {
+        return false;
+    }
+
+    return recur(left.right, right.left) && recur(left.left, right.right);
+}
+
+var isSymmetric = function(root) {
+    return recur(root.left, root.right);
+};
