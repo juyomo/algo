e42ac4f3561b5aba9511008cd4854c6539b62aee
juyomo <juyomo@users.noreply.github.com>
2025-07-07 17:59:57 +0900

    Update 2025_07_08_4_subtree_of_another_tree.js

--- 2025_07_08/2025_07_08_4_subtree_of_another_tree.js
+++ 2025_07_08/2025_07_08_4_subtree_of_another_tree.js
@@ -7,3 +7,38 @@
 // 2. Convert Sorted Array to Binary Search Tree
 // 3. Reverse Bits
 // 4. Subtree of Another Tree
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+
+function sameTree(a, b) {
+    if (a == null && b == null) {
+        return true;
+    } else if (a == null || b == null) {
+        return false;
+    } else if (a.val != b.val) {
+        return false;
+    }
+
+    return sameTree(a.left, b.left) && sameTree(a.right, b.right);
+}
+
+var isSubtree = function(root, subRoot) {
+    if (root == null && subRoot == null) {
+        return true;
+    } else if (root == null || subRoot == null) {
+        return false;
+    } 
+    return sameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
