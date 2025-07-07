41e7610e00c24cb9fa105ba5b4bf0714a0b009e6
juyomo <juyomo@users.noreply.github.com>
2025-07-07 18:05:39 +0900

    Update 2025_07_08_1_palindrome_number.cpp

--- 2025_07_08/2025_07_08_1_palindrome_number.cpp
+++ 2025_07_08/2025_07_08_1_palindrome_number.cpp
@@ -7,3 +7,26 @@
 // 2. Convert Sorted Array to Binary Search Tree
 // 3. Reverse Bits
 // 4. Subtree of Another Tree
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        } else if (x < 10) {
+            return true;
+        }
+        vector<int> v;
+        while (x > 0) {
+            v.push_back(x % 10);
+            x /= 10;
+        }
+        int len = v.size();
+        for (int i = 0; i < len; i++) {
+            if (v[i] != v[len - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
