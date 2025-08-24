b790ab5976aa58dfc18de8e7898dd10c4d79ab6e
juyomo <juyomo@users.noreply.github.com>
2025-08-24 23:40:10 +0900

    Update 2025_08_26_1_evaluate_rpn.cpp

--- 2025_08_26/2025_08_26_1_evaluate_rpn.cpp
+++ 2025_08_26/2025_08_26_1_evaluate_rpn.cpp
@@ -6,3 +6,37 @@
 // 1. Evaluate Reverse Polish Notation
 // 2. Course Schedule
 
+class Solution {
+public:
+    void helper(stack<int>& stk, int& a, int& b) {
+        if (stk.size() < 2) {
+            return;
+        }
+
+        b = stk.top(); stk.pop();
+        a = stk.top(); stk.pop();
+    }
+
+    int evalRPN(vector<string>& tokens) {
+        stack<int> nums;
+        int a, b;
+        for (string s : tokens) {
+            if (s == "*") {
+                helper(nums, a, b);
+                nums.push(a * b);
+            } else if (s == "/") {
+                helper(nums, a, b);
+                nums.push(a / b);
+            } else if (s == "+") {
+                helper(nums, a, b);
+                nums.push(a + b);
+            } else if (s == "-") {
+                helper(nums, a, b);
+                nums.push(a - b);
+            } else {
+                nums.push(stoi(s));
+            }
+        }
+        return nums.top();
+    }
+};
